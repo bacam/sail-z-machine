c512d774b16a5f42ef1cb999bbb16bde4f4bb967
Brian Campbell <bacam@z273.org.uk>
2025-01-18 12:05:44 +0000

    Instructions: je, rtrue, rfalse

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-(cd src; sail -c --c-no-main -o ../zmachine prelude.sail base.sail list.sail memory.sail zstring.sail instructions.sail fetch_decode.sail init.sail main.sail memory_interface.sail -c_include extralib.h)
+(cd src; sail -c --c-no-main -o ../zmachine prelude.sail base.sail list.sail memory.sail zstring.sail routine.sail instructions.sail fetch_decode.sail init.sail main.sail memory_interface.sail -c_include extralib.h)
 SAIL_DIR=`sail --dir`
 gcc -g src/extralib.c zmachine.c -I src "$SAIL_DIR"/lib/*.c -lgmp -lz -I "$SAIL_DIR"/lib/ src/main.c -o zmachine
